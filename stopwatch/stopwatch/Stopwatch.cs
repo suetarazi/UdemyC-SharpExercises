5b5c5a1fd488a7b14a8b90d083033a2199703644
Sue Tarazi <suemachtley@gmail.com>
2020-06-23 17:24:56 -0700

    stopwatch - initial commit

---
+++ stopwatch/stopwatch/Stopwatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stopwatch
+{
+    class Stopwatch
+    {
+        public TimeSpan Start()
+        {
+            
+        }
+
+        public TimeSpan Stop()
+        {
+
+        }
+    }
+}
