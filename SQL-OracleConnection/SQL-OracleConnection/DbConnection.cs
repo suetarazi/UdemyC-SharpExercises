3eb8aa2e9daec0d920207caf2c5e1363fdfab8dd
Sue Tarazi <suemachtley@gmail.com>
2020-06-29 20:41:29 -0700

    getters & setters, constructor

---
+++ SQL-OracleConnection/SQL-OracleConnection/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace SQL_OracleConnection
+{
+    class DbConnection
+    {
+        public string ConnectionString {get; set;}
+        public TimeSpan TimeOut { get; set; }
+
+        public void DatabaseConnection()
+        {
+            DbConnection connection = new DbConnection();
+
+        }
+    }
+}
