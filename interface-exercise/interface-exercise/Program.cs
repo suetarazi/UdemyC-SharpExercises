b7aa762bd4a3774d5171f938a3d51d50a3b09ab8
Sue Tarazi <suemachtley@gmail.com>
2020-07-01 19:13:05 -0700

    interfaces

---
+++ interface-exercise/interface-exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace interface_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Object workflow = new Object();
+            workflow.Run();
+        }
+
+        static void Run()
+        {
+
+        }
+    }
+}
